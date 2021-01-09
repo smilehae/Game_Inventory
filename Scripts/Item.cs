31cc96d5f0918305e879907c8a6dee6f251d0824
smilehae <mihaepyun@gmail.com>
2021-01-09 13:51:08 +0900

    아이템 데이터베이스 만들기

---
+++ Scripts/Item.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public int id;
+    public string name;
+    public string description;
+    public Sprite icon;
+    //power은 16이다, 이동속도가 증가한다 이런거 넣으려고 key와 value가 있는 dictionary 사용
+
+    public Dictionary<string, int> stats = new Dictionary<string, int>();
+
+    public Item(int id, string name, string desc, Dictionary<string, int> stats) {
+        this.id = id;
+        this.name = name;
+        this.description = desc;
+        this.stats = stats;
+
+        this.icon = Resources.Load<Sprite>("Sprites/Items/" + name);
+
+    }
+
+    public Item(Item item)
+    {
+        this.id = item.id;
+        this.name = item.name;
+        this.description = item.description;
+        this.stats = item.stats;
+
+        this.icon = Resources.Load<Sprite>("Sprites/Items/" + item.name);
+
+    }
+}
---
+++ Scripts/ItemDatabase.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Awake() {
+        BuildDatabase();
+    }
+
+    public Item GetItem(int id) {
+        return items.Find(item => item.id == id);
+    }
+
+    public Item GetItem(string itemName) {
+        return items.Find(item => item.name == itemName);
+    }
+
+    void BuildDatabase()
+    {
+        items = new List<Item>() {
+
+            new Item(0,"watermelon","it is delicious.",
+            new Dictionary<string, int>{
+                { "power",15 },{ "Defence",10}
+            }),
+
+              new Item(1,"strawberry","it is sweet.",
+            new Dictionary<string, int>{
+                { "power",10 },{ "Defence",50}
+            })
+
+
+        };
+
+
+    }
+}
